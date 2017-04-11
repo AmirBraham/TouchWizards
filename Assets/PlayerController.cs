65f72f721785a76717ca7f1a3a624ac83fed1706
Amir Braham <amirbrahamm@gmail.com>
2017-04-11 18:59:58 +0100

    Added Player Controller
    Checks for ground , Ability to move with keyboard or JoyStick

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;             //Floating point variable to store the player's movement speed.
+    public float jumpSpeed;
+    bool canDoubleJump;
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public GameObject point;
+    public float radius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        
+
+        //check if the player in on ground
+        isGrounded = Physics2D.OverlapCircle(point.transform.position,radius,whatIsGround);
+       
+        
+    }
+
+    void Update () {
+        if        (CnInputManager.GetAxisRaw("Horizontal") > 0) {
+                rb2d.velocity = new Vector2(moveSpeed,rb2d.velocity.y);
+                //GetComponent<Animator>().SetBool("isMoving",true);
+                transform.localScale = new Vector3(1,1,1);
+
+        } else if (CnInputManager.GetAxisRaw("Horizontal") < 0) {
+                rb2d.velocity = new Vector2(-moveSpeed,rb2d.velocity.y);
+                //GetComponent<Animator>().SetBool("isMoving",true);
+                transform.localScale = new Vector3(-1,1,1);
+                
+        } else {
+                rb2d.velocity = new Vector2(0,rb2d.velocity.y);
+           // GetComponent<Animator>().SetBool("isMoving",false);
+                
+        }
+
+        if(CnInputManager.GetAxisRaw("Vertical") > 0) {
+            
+            if(isGrounded) {
+                rb2d.velocity = new Vector2(rb2d.velocity.x,jumpSpeed);
+                                //GetComponent<Animator>().SetBool("isJumping",true); 
+
+            } 
+                
+
+            } else {
+                 //GetComponent<Animator>().SetBool("isJumping",false); 
+
+            }
+        }
+    
+
+     void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(point.transform.position, radius);
+
+    }
+}

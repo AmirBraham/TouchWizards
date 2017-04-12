ad8439853cb3e901b71ca403ac9072be07bc489d
Amir Braham <amirbrahamm@gmail.com>
2017-04-12 12:33:32 +0100

    Addeded physics to Players bullets

---
+++ Assets/P1_shoot_Movement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_shoot_Movement : MonoBehaviour {
+
+	public float bulletSpeed;
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0,bulletSpeed);
+	}
+}

412eb85bf9bd19df5cfc9f57ba5f3c8cd27eadb0
Amir Braham <amirbrahamm@gmail.com>
2017-04-12 16:49:17 +0100

    Created Home Screen With Play and Credits buttons

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SceneManager : MonoBehaviour {
+	public GameObject CREDITS;
+	public GameObject PLAY;
+
+	public GameObject CreditsText;
+
+	public GameObject ReturnUI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GameScene () {
+		Application.LoadLevel("Main");
+	}
+
+	public void Credits () {
+		PLAY.transform.DOMoveX(3000,2f);
+		CREDITS.transform.DOMoveX(-1000,2f);
+		CreditsText.transform.DOLocalMoveX(0,3f);
+	}
+
+	public void Return () {
+		PLAY.transform.DOMoveX(550,2f);
+		CREDITS.transform.DOMoveX(550,2f);
+		CreditsText.transform.DOLocalMoveX(2001f,2f);
+
+
+	}
+}
